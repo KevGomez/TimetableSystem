0493f98027de8a148423221f53f2ae662f6ccfc0
DilshanHarendra <harendracripto@gmail.com>
2020-09-23 01:06:48 +0530

    Clean Project

--- src/timetablesystem/DataBaseHandler/CreateTables.java
+++ src/timetablesystem/DataBaseHandler/CreateTables.java
@@ -2,8 +2,4 @@ package timetablesystem.DataBaseHandler;
 
 public class CreateTables {
     static String timeSolts="CREATE TABLE IF NOT EXISTs timeslots ( id int PRIMARY KEY AUTO_INCREMENT ,tSlots varchar(100) )";
-
-
-
-
 }
