5e8dd54002d818c0199936c23cf5e028ddfd02ab
VidulaDakshitha <viduladakshitha@gmail.com>
2020-09-30 07:25:49 +0530

    added get lecturers

--- src/Controller/SessionController.java
+++ src/Controller/SessionController.java
@@ -5,9 +5,15 @@
  */
 package Controller;
 
+import Model.LecturerDB;
+import Model.LecturerModel;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +22,50 @@ import javafx.fxml.Initializable;
  */
 public class SessionController implements Initializable {
 
+    @FXML
+    
+    private ComboBox lecturers;
+    
+      @FXML
+    
+    private ComboBox tags;
+      
+        @FXML
+    
+    private ComboBox groups;
+        
+          @FXML
+    
+    private ComboBox subject;
+    
+          
+             @FXML
+    
+    private ComboBox subgroups;
+    
+    
+      @FXML
+    private TextField noOfStudents;
+      
+           @FXML
+    private TextField duration;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        getDataLecturers();
+    }
+    
+    
+    public void getDataLecturers(){
+        
+        LecturerDB l2=new LecturerDB();
+        ObservableList<LecturerModel> list = l2.getAllData();
+        
+        lecturers.setItems(list);
+        
+    }
     
 }
